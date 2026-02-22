efc01202f7970d9ca6c847fdb583ee5532b8d753
sivanagarajugogasani <151191189+sivanagarajugogasani@users.noreply.github.com>
2026-02-22 18:19:35 +0530

    Test file

--- tests/DemoWebShop.spec.ts
+++ tests/DemoWebShop.spec.ts
@@ -3,5 +3,6 @@ test('Demo web shop', async({page})=>{
 
 
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.goto('');
     await page.waitForTimeout(3000);
 })
