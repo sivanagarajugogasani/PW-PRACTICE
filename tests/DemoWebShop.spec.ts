2448f25d2050a180f8d941c955a1f4ee51f259ce
sivanagarajugogasani <151191189+sivanagarajugogasani@users.noreply.github.com>
2026-02-22 13:42:55 +0530

    Test file

--- tests/DemoWebShop.spec.ts
+++ tests/DemoWebShop.spec.ts
@@ -2,6 +2,6 @@ import {test,expect} from '@playwright/test';
 test('Demo web shop', async({page})=>{
 
 
-    await page.goto('https://demowebshop.tricentis.com/login.');
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
     await page.waitForTimeout(3000);
 })
